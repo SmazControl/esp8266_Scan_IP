14be1ca549e67d9fdf59c2e421bd7c25a32f0f85
Bunnavit Sawangpiriyakij <smazcontrol@gmail.com>
2019-12-24 19:02:26 +0700

    Add files via upload

--- client_scan/ESP8266_STA_client.ino
+++ client_scan/ESP8266_STA_client.ino
@@ -8,47 +8,62 @@
 #define USE_SERIAL Serial
 
 ESP8266WiFiMulti WiFiMulti;
-asyncHTTPrequest request[100];
 
 int break_state = 0;
 String url1;
+asyncHTTPrequest request[85];
+String response;
 
 void requestCB(void* optParm, asyncHTTPrequest* request, int readyState){
-    if(readyState == 4 && request->responseText()!=""){
-        Serial.println(request->responseText());
-        Serial.println("out");
-        request->setDebug(false);
-		    break_state = 1;
+    response = request->responseText();
+    Serial.println(response);
+    Serial.println("out");
+    request->setDebug(false);
+		break_state = 1;
+}
+
+void scan(int start, int quan) {
+  int time;
+  int j=0;
+  for (int i=start; i<start+quan; i++) {
+    request[j].onData(requestCB);
+    request[j].setTimeout(2);
+    time = millis();
+    String url = "http://192.168.43."+String(i)+"/get";
+    url1 = "192.168.1."+String(i);
+    request[j].open("GET", url.c_str());
+    request[j].send();
+    Serial.print(url);
+    while (time+500>millis()) {
+      Serial.print(".");
+      delay(100);
     }
+    if (break_state == 1) {
+      break;
+    }
+    Serial.println("/");
+    j+=1;
+  }
+  if (break_state != 1) {
+    delay(1000);
+  }  
 }
 
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
-  WiFi.begin("phloenlom_2.4GHz", "248248248");
+  WiFi.begin("Supod", "palace66");
   while (WiFi.status() != WL_CONNECTED) {
 	  Serial.print(".");
     delay(200);
   }
   Serial.println(" ");
-  int time;
-  for (int i=2; i<100; i++) {
-    request[i].onReadyStateChange(requestCB);
-    request[i].setTimeout(2);
-	  time = millis();
-    String url = "http://192.168.1."+String(i)+"/get";
-    url1 = "192.168.1."+String(i);
-    request[i].open("GET", url.c_str());
-    request[i].send();
-    Serial.print(url);
-  	while (time+500>millis()) {
-      Serial.print(".");
-	  	delay(100);
-	  }
-	  if (break_state == 1) {
-		  break;
-	  }
-    Serial.println("/");
+  scan(2,81);
+  if (break_state!=1) {
+    scan(83,81);
+  }
+  if (break_state!=1) {
+    scan(164,81);
   }
 }
 
@@ -56,7 +71,12 @@ void loop() {
   Serial.println(" ");
   Serial.println("IP_SCAN");
   Serial.println("==============");
-  Serial.println(url1);
+  if (break_state==1) {
+    Serial.println(response);
+  } else {
+    Serial.println("IP_SCAN Not found");
+  }
+  
   while (true) {
     delay(100);
   }
